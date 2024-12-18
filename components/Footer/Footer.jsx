b122101a8c30af75f7eaf151d0959b9b95d31abe
Ayan Alam <ayanalam0025@gmail.com>
2024-12-18 20:27:39 +0530

    first commit

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native"
+import { s } from "./Footer.style";
+export function Footer(){
+    return <Text style={s.footer}>Developed by Ayan Alam</Text>;
+}
---
+++ components/Footer/Footer.style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  footer: {
+    color: "white",
+    fontWeight: "300",
+    paddingTop: 20,
+    position: "absolute",
+    marginTop: 480,
+    opacity: 0.5,
+  },
+});
